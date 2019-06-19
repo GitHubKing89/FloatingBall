5186a27bbef905ae1b959d6aec01fc9e180f4364
machao <machao01@skyworth.com>
2019-06-19 18:51:56 +0800

    start code commit

--- app/src/main/java/com/floating/qihang/MainActivity.java
+++ app/src/main/java/com/floating/qihang/MainActivity.java
@@ -2,6 +2,7 @@ package com.floating.qihang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View view = new View(this);
     }
 }
